7dfab1c66dea0c98534d4ac4ee65290debf6d303
JasonEl1 <1eliseijas@hdsb.ca>
2025-11-20 01:21:11 -0500

    Add custom start/end dates & scatter line fit

--- line.py
+++ line.py
@@ -1,4 +1,4 @@
-#v0.3
+#v0.4
 
 import analysis
 import habo
@@ -6,16 +6,18 @@ import daylio
 from datetime import datetime,timedelta
 import numpy as np
 import math
+import sys
+import matplotlib.pyplot as plt
 
 HABO_FILENAME=habo.get_filename()
 DAYLIO_FILENAME=daylio.get_filename()
 
 def rolling_average(list):
-    CHUNK_SIZE=math.ceil(len(list)/100)
+    CHUNK_SIZE=math.ceil(len(list)/50)
 
     newlist=[]
     for i in range(0,len(list),CHUNK_SIZE):
-        newlist.append(sum(list[i:i+CHUNK_SIZE])/CHUNK_SIZE)
+        newlist.append(sum(list[i:i+CHUNK_SIZE])/len(list[i:i+CHUNK_SIZE]))
 
     list[:]=newlist
 
@@ -23,20 +25,46 @@ print("Analyzing data...")
 
 start_end_dates=analysis.get_start_end_days(HABO_FILENAME,DAYLIO_FILENAME)
 
-start_date = start_end_dates[0]
-end_date = start_end_dates[1]
+if(len(sys.argv)==3 and len(sys.argv[1])==len(sys.argv[2]) and len(sys.argv[1])==10): #if correctly passed start, end dates
+    try:
+        start_date=sys.argv[1]
+        date=datetime.strptime(start_date,"%Y-%m-%d")
+        end_date=sys.argv[2]
+
+    except ValueError:
+        print("Incorrect start date format")
+        sys.exit()
+
+    try:
+        assert((date-datetime.strptime(start_end_dates[0],"%Y-%m-%d")).days >=0)
+    except:
+        print(f"Error: Start date is not within data range\nAllowed range is: {start_end_dates[0]} to {start_end_dates[1]}")
+        sys.exit()
+
+    try:
+        assert((datetime.strptime(end_date,"%Y-%m-%d")-datetime.strptime(start_end_dates[1],"%Y-%m-%d")).days <=0)
+    except:
+        print(f"Error: End date is not within data range\nAllowed range is: {start_end_dates[0]} to {start_end_dates[1]}")
+        sys.exit()
+
+    num_days=(datetime.strptime(end_date,"%Y-%m-%d")-date).days + 1
+else:
+    start_date = start_end_dates[0]
+    end_date = start_end_dates[1]
+
+    date=datetime.strptime(start_date, "%Y-%m-%d")
+
+    num_days=analysis.get_total_days(HABO_FILENAME,DAYLIO_FILENAME)
 
 y_daylio=[]
 y_habo=[]
-date=datetime.strptime(start_date, "%Y-%m-%d")
-for i in range(analysis.get_total_days(HABO_FILENAME,DAYLIO_FILENAME)):
+
+for i in range(num_days):
     y_daylio.append(daylio.get_mood_for_date(date.strftime("%Y-%m-%d"),DAYLIO_FILENAME))
     y_habo.append(habo.get_completion_for_day(date.strftime("%Y-%m-%d"),HABO_FILENAME))
     date+=timedelta(days=1)
 
 print("Generating line plot...")
-
-import matplotlib.pyplot as plt
 
 assert(len(y_daylio)!=0 and len(y_habo)!=0)
 rolling_average(y_daylio)
@@ -46,7 +74,7 @@ x = np.arange(0, len(y_daylio), 1)
 fig,daylio_axis=plt.subplots()
 daylio_line = daylio_axis.plot(x,y_daylio,label="Daylio",color="blue")
 daylio_axis.set_ylabel("Mood")
-daylio_axis.set_xticks([x[50],x[len(x)-10]],labels=["2024-11-04", "2025-11-04"]) #needs update
+daylio_axis.set_xticks([x[0],x[len(x)-1]],labels=[start_date,end_date])
 daylio_axis.set_xlabel("Date")
 
 habo_axis = daylio_axis.twinx()
--- scatter.py
+++ scatter.py
@@ -1,33 +1,72 @@
-#v0.2
+#v0.3
 
 import analysis
 import habo
 import daylio
 from datetime import datetime,timedelta
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
 
 HABO_FILENAME=habo.get_filename()
 DAYLIO_FILENAME=daylio.get_filename()
 
 print("Analyzing data...")
 
+start_end_dates=analysis.get_start_end_days(HABO_FILENAME,DAYLIO_FILENAME)
+
+if(len(sys.argv)==3 and len(sys.argv[1])==len(sys.argv[2]) and len(sys.argv[1])==10): #if correctly passed start, end dates
+    try:
+        start_date=sys.argv[1]
+        date=datetime.strptime(start_date,"%Y-%m-%d")
+        end_date=sys.argv[2]
+
+    except ValueError:
+        print("Incorrect start date format")
+        sys.exit()
+
+    try:
+        assert((date-datetime.strptime(start_end_dates[0],"%Y-%m-%d")).days >=0)
+    except:
+        print(f"Error: Start date is not within data range\nAllowed range is: {start_end_dates[0]} to {start_end_dates[1]}")
+        sys.exit()
+
+    try:
+        assert((datetime.strptime(end_date,"%Y-%m-%d")-datetime.strptime(start_end_dates[1],"%Y-%m-%d")).days <=0)
+    except:
+        print(f"Error: End date is not within data range\nAllowed range is: {start_end_dates[0]} to {start_end_dates[1]}")
+        sys.exit()
+
+    num_days=(datetime.strptime(end_date,"%Y-%m-%d")-date).days + 1
+else:
+    start_date = start_end_dates[0]
+    end_date = start_end_dates[1]
+
+    date=datetime.strptime(start_date, "%Y-%m-%d")
+
+    num_days=analysis.get_total_days(HABO_FILENAME,DAYLIO_FILENAME)
+
 data_x=[]
 data_y=[]
 
-date=datetime.strptime("2024-10-01", "%Y-%m-%d")
-for i in range(analysis.get_total_days(HABO_FILENAME,DAYLIO_FILENAME)):
+for i in range(num_days):
     data_x.append(daylio.get_mood_for_date(date.strftime("%Y-%m-%d"),DAYLIO_FILENAME))
     data_y.append(habo.get_completion_for_day(date.strftime("%Y-%m-%d"),HABO_FILENAME))
     date+=timedelta(days=1)
 
 print("Generating scatter plot...")
 
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import numpy as np
-
 daylio_cmap = ListedColormap(daylio.DAYLIO_MOOD_COLOURS)
 
+m,b=np.polyfit(data_x,data_y,1)
+print(m)
+print(b)
+print(data_x[0])
+x=np.array([1,2,3,4,5])
+
 plt.scatter(data_x, data_y,c=data_x,cmap=daylio_cmap)
+plt.plot(x,m*x+b,alpha=0.1)
 plt.ylim(0.0, 1.2)
 plt.xticks(list(daylio.DAYLIO_MOOD_TO_INT.values()),labels=list(daylio.DAYLIO_MOOD_TO_INT.keys()))
 plt.yticks([0.0,0.2,0.4,0.6,0.8,1.0])
